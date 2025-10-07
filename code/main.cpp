1d71159486556d1dec279cb8d1f16d5e94ade830
HaThaiCT <cthtzz@gmail.com>
2025-10-07 08:40:31 +0700

    update code

--- code/main.cpp
+++ code/main.cpp
@@ -1,40 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-
-    int thang, khoinuoc;
-    int tiennuoc;
-    cout << "Nhap thang:";
-    cin >> thang;
-    cout << "Nhap so khoi nuoc da tieu thu:";
-    cin >> khoinuoc;
+int tiennuoc(int thang, int khoinuoc) {
+    int res;
     if(thang >= 1 && thang <= 6) {
         if (khoinuoc <= 10 && khoinuoc >= 0) {
-            tiennuoc = 36000 * khoinuoc;
+            res = 36000 * khoinuoc;
         } else if(khoinuoc > 10) {
-            tiennuoc = 30000 * khoinuoc;
+            res = 30000 * khoinuoc;
         } else {
             cout << "Dau vao khong hop le!";
-            return 0;
+            return -1;
         }
     }
     else if (thang >= 7 && thang <= 12) {
         if (khoinuoc <= 10 && khoinuoc >= 0) {
-            tiennuoc = 33000 * khoinuoc;
+            res = 33000 * khoinuoc;
         } else if(khoinuoc > 10) {
-            tiennuoc = 26000 * khoinuoc;
+            res = 26000 * khoinuoc;
         } 
         else {
             cout << "Dau vao khong hop le!";
-            return 0;            
+            return -1;            
         }
     }
     else {
         cout << "Dau vao khong hop le!";
-        return 0;
+        return -1;
     }
+}
 
-    cout << "Hoa don tien nuoc thang nay la: " << tiennuoc << " dong";
+
+int main() {
+
+    int thang, khoinuoc;
+    cout << "Nhap thang:";
+    cin >> thang;
+    cout << "Nhap so khoi nuoc da tieu thu:";
+    cin >> khoinuoc;
+    cout << "Hoa don tien nuoc thang nay la: " << tiennuoc(thang, khoinuoc) << " dong";
     return 0;
 }
